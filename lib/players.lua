934a6e098396d531966d9f8cbad2fb9fadb773ed
avtoradio48 <danil94ss@gmail.com>
2025-07-29 16:41:32 +0700

    Create players.lua

---
+++ lib/players.lua
@@ -0,0 +1,10 @@
+local players = {}
+
+-- Placeholder: retrieve tracked player activity
+-- Should return a list of { name = "playerName", online = true/false }
+function players.getTracked()
+  -- TODO: implement real detection (e.g., via event listening or player_detector component)
+  return {}
+end
+
+return players
